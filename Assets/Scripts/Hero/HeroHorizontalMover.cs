54f2bdb678254c7202bd270c26ffb483219f4f97
Lakello <59009343+Lakello@users.noreply.github.com>
2023-10-16 14:08:18 +0700

    refactoring

---
+++ Assets/Scripts/Hero/HeroHorizontalMover.cs
@@ -0,0 +1,39 @@
+﻿using Game.Input;
+using Reflex.Attributes;
+using UnityEngine;
+
+namespace Game.Hero
+{
+    public class HeroHorizontalMover : HeroMover
+    {
+        [SerializeField] private float _radius;
+
+        protected override void OnMove(float direction)
+        {
+            if (MoveCoroutine == null)
+            {
+                var targetTransform = Target.transform;
+                var playerPosition = transform.position;
+
+                var startAngleTarget = Target.transform.rotation.y;
+                float angleCurrent = 0;
+
+                var radius = Vector3.Distance(playerPosition, targetTransform.position);
+                var angleTarget = (DistanceMove * 360) / (2 * Mathf.PI * radius);
+
+                MoveCoroutine = StartCoroutine(Move((currentTime) =>
+                {
+                    angleCurrent += Mathf.Lerp(startAngleTarget, angleTarget, currentTime / TimeToTarget);
+                    return (Quaternion.AngleAxis(angleCurrent * direction, Vector3.up) * new Vector3(radius, 0, 0)) + targetTransform.position;
+                }));
+            }
+        }
+
+        [Inject]
+        protected override void Inject(IInputHandler handler)
+        {
+            InputHandler = handler;
+            InputHandler.Horizontal += OnMove;
+        }
+    }
+}
---
+++ Assets/Scripts/Hero/HeroMover.cs
@@ -0,0 +1,51 @@
+﻿using Game.Input;
+using System.Collections;
+using UnityEngine;
+
+namespace Game.Hero
+{
+    public abstract class HeroMover : MonoBehaviour
+    {
+        [SerializeField] protected float DistanceMove;
+        [SerializeField] protected float TimeToTarget;
+        [SerializeField] protected GameObject Target;
+
+        protected Coroutine MoveCoroutine;
+        protected IInputHandler InputHandler;
+
+        private void OnEnable()
+        {
+            if (InputHandler != null)
+            {
+                InputHandler.Vertical += OnMove;
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (InputHandler != null)
+            {
+                InputHandler.Vertical -= OnMove;
+            }
+        }
+
+        protected abstract void Inject(IInputHandler inputHandler);
+        protected abstract void OnMove(float direction);
+
+        protected IEnumerator Move(System.Func<float, Vector3> calculatePosition)
+        {
+            float currentTime = 0;
+
+            while (currentTime <= TimeToTarget)
+            {
+                transform.position = calculatePosition(currentTime);
+
+                currentTime += Time.deltaTime;
+
+                yield return null;
+            }
+
+            MoveCoroutine = null;
+        }
+    }
+}
