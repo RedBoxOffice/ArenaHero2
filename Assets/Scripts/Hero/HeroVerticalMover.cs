40daf0af9e63bbf8d9be7505463f4e8adcc2b557
Lakello <59009343+Lakello@users.noreply.github.com>
2023-10-16 13:57:13 +0700

    refactoring

---
+++ Assets/Scripts/Hero/HeroVerticalMover.cs
@@ -0,0 +1,34 @@
+﻿using Game.Input;
+using Reflex.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Game.Hero
+{
+    public class HeroVerticalMover : HeroMover
+    {
+        [Inject]
+        protected override void Inject(IInputHandler handler)
+        {
+            InputHandler = handler;
+            InputHandler.Vertical += OnMove;
+        }
+
+        protected override void OnMove(float direction)
+        {
+            if (MoveCoroutine == null)
+            {
+                Vector3 startPosition = transform.position;
+
+                var targetPosition = transform.position + (Vector3.forward * direction * DistanceMove);
+
+                MoveCoroutine = StartCoroutine(Move((currentTime) =>
+                    Vector3.Lerp(startPosition, targetPosition, currentTime / TimeToTarget)));
+            }
+        }
+    }
+}
