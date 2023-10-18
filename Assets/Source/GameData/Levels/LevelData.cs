fc3628c9ef4c5ad9e9f0d3f4f975be188668da6c
Lakello <59009343+Lakello@users.noreply.github.com>
2023-10-18 16:46:48 +0700

    Рефакторинг архитектуры

---
+++ Assets/Source/GameData/Levels/LevelData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace GameData
+{
+    [CreateAssetMenu(menuName = "Level/New Level", fileName = "level")]
+    public class LevelData : ScriptableObject
+    {
+        [SerializeField] private WaveData[] _waveData;
+
+        public WaveData GetWaveData(int index) => _waveData[index];
+        public int WaveCount => _waveData.Length;
+    }
+}
---
+++ Assets/Source/Scripts/Fight/Enemies/Spawner/SpawnerHandler.cs
@@ -0,0 +1,37 @@
+﻿using Base.Object;
+using GameData;
+using Reflex.Attributes;
+using System;
+using UnityEngine;
+
+namespace Game.Enemies
+{
+    [RequireComponent(typeof(SpawnPointsHandler))]
+    public class SpawnerHandler : MonoBehaviour
+    {
+        private WaveHandler _waveHandler;
+        private SpawnPointsHandler _spawnPointsHandler;
+        private ObjectSpawner<EnemyInit> _spawner;
+
+        private void Awake()
+        {
+            _spawnPointsHandler = GetComponent<SpawnPointsHandler>();
+
+            _spawner = new(
+                new ObjectFactory<EnemyInit>(new GameObject(nameof(EnemyInit)).transform),
+                new ObjectPool<EnemyInit>());
+        }
+
+        [Inject]
+        private void Inject(WaveHandler waveHandler)
+        {
+            _waveHandler = waveHandler;
+            _waveHandler.Spawn += OnSpawn;
+        }
+
+        private void OnSpawn(Enemy enemy)
+        {
+            //_spawner.Spawn(enemy, () => _spawnPointsHandler.GetSpawnPosition());
+        }
+    }
+}
